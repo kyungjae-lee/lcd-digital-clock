02ca80796bd9a95731856a01e63cc846396197a7
Kyungjae Lee <kjxlee@gmail.com>
2023-07-17 07:59:39 -0500

    Create 'workspace/lcd_digital_clock/' and its folder hierarchy

---
+++ workspace/lcd_digital_clock/Src/main.c
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * File		: main.c
+ * Brief	: Application to read time and date information from the Tiny RTC
+ * 			  Module (DS1307) and display it on the 16x2 Character LCD (HD4478U)
+ * Author	; Kyungjae Lee
+ * Date		: Jul 17, 2023
+ * ****************************************************************************/
+
+int main(int argc, char *argv[])
+{
+	while (1);
+
+	return 0;
+} /* End of main */
