1369acb55fcb8a1fe14a29f8054191ab00ee4aad
Kyungjae Lee <kjxlee@gmail.com>
2023-07-18 08:31:06 -0500

    Update 'workspace/lcd_digital_clock/' - Create stm32f407xx MCU peripheral drivers

--- workspace/lcd_digital_clock/Driver/Inc/stm32f407xx_rcc_driver.h
+++ workspace/lcd_digital_clock/Driver/Inc/stm32f407xx_rcc_driver.h
@@ -1,6 +1,23 @@
 /*******************************************************************************
  * File		: stm32f407xx_rcc_driver.h
- * Brief	:
+ * Brief	: STM32F407xx MCU specific RCC driver header file
  * Author	; Kyungjae Lee
  * Date		: Jul 17, 2023
  * ****************************************************************************/
+
+#ifndef STM32F407XX_RCC_DRIVER_H
+#define STM32F407XX_RCC_DRIVER_H
+
+#include "stm32f407xx.h"
+
+/*******************************************************************************
+ * APIs supported by the SPI driver
+ * (See function definitions for more information)
+ ******************************************************************************/
+
+uint32_t RCC_GetPCLK1Value(void);
+uint32_t RCC_GetPCLK2Value(void);
+uint32_t RCC_GetPLLOutputClk(void);
+
+
+#endif /* STM32F407XX_RCC_DRIVER_H */
